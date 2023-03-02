5044d035532921b821a0e0304201990bc0dc6ed4
0gl04q <115027096+0gl04q@users.noreply.github.com>
2023-03-02 10:39:53 +0300

    normalization ver 0.1

---
+++ gu.py
@@ -0,0 +1,20 @@
+# авторизация на Госуслугах
+    # driver.get("https://esia.gosuslugi.ru/login/")
+    #
+    # time.sleep(2)
+    # driver.find_element(By.XPATH, '/html/body/esia-root/div/esia-login/div/div[1]/form/div[4]/div/div[2]/div/button').click()
+    #
+    # time.sleep(2)
+    # driver.find_element(By.XPATH,
+    #                     '/html/body/esia-root/div/esia-login/div/div[1]/esia-eds/button').click()
+    #
+    # # тест
+    # time.sleep(10)
+    # driver.find_element(By.XPATH,
+    #                     '/html/body/esia-root/esia-modal-placeholder/div/div[2]/div/div/div/ng-component/div/esia-eds-item[1]').click()
+    #
+    #
+    # time.sleep(20)
+    # driver.find_element(By.XPATH,
+    #                     '/html/body/esia-root/esia-modal-placeholder/div/div[2]/div/div/div/ng-component/div/div[2]/a').click()
+    # time.sleep(5)
