1d59c9b099df83cd571d30845920c40b558fa22e
0gl04q <2000danilrusan2000@gmail.com>
2023-03-30 15:46:45 +0300

    add check chern/publ and dowload file RA

--- Bot_LK_RA.py
+++ Bot_LK_RA.py
@@ -1,6 +1,9 @@
+import datetime
 import time
 import os
 import shutil
+
+from tkinter import messagebox
 
 from openpyxl import load_workbook
 from threading import Thread
@@ -257,11 +260,29 @@ def wait_att(d, paste):
         wait_att(d, paste)
 
 
+# Ожидание появления необходимой даты в таблице
+def wait_date(d, paste):
+    try:
+        WebDriverWait(d, 60).until(
+            EC.text_to_be_present_in_element((By.XPATH, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification'
+                                                        '-measuring-instruments/div/div/div[2]/fgis-table/div['
+                                                        '2]/div/table/tbody/a[1]/td[6]/div/div[1]/span'), paste))
+        WebDriverWait(d, 60).until(
+            EC.text_to_be_present_in_element((By.XPATH, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification'
+                                                        '-measuring-instruments/div/div/div[2]/fgis-table/div['
+                                                        '2]/div/table/tbody/a[2]/td[6]/div/div[1]/span'), paste))
+
+        return d.find_element(By.XPATH, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring'
+                                        '-instruments/div/div/div[3]/fgis-table-paging/div/div[1]/div['
+                                        '2]').text.split()[1]
+    except Te:
+        wait_date(d, paste)
+
+
 # Функция публикации сведения в РА
 def public_info(d, num_res):
-
     # Сообщение для пользователя
-    print(f'Приступаю к публикации сведения {num_res}')
+    print(f'Публикую сведения {num_res}')
 
     # Переход в рабочую область
     work_source(d)
@@ -367,7 +388,6 @@ def authorization_ra(d, org):
 
 # Функция для определения продолжить работу или начать авторизацию
 def all_authorization(d, org):
-
     # переход в рабочую область
     d.get('http://10.250.74.17/roei/verification-measuring-instruments')
 
@@ -419,10 +439,153 @@ def save_wb(wb, file):
         save_wb(wb, file)
 
 
+def check_kol(d, my_date, obsh, prom, file):
+    # Переход в рабочую область
+    work_source(d)
+
+    time.sleep(2)  # ожидание
+
+    # Внесение с "Дата поверки"
+    wait_send_keys(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/div/fgis'
+                      '-roei-verification-measuring-instruments-advanced-search/fgis-filters-panel/fgis-left'
+                      '-panel/div[2]/div[2]/div/div[2]/fgis-date-range/div/fgis-calendar[1]/div/div/input',
+                   my_date)
+
+    # Внесение по "Дата поверки"
+    wait_send_keys(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/div/fgis'
+                      '-roei-verification-measuring-instruments-advanced-search/fgis-filters-panel/fgis-left'
+                      '-panel/div[2]/div[2]/div/div[2]/fgis-date-range/div/fgis-calendar[2]/div/div/input',
+                   my_date)
+
+    # Включить отображение кол-ва поверок
+    wait_click(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/div/fgis-roei'
+                  '-verification-measuring-instruments-advanced-search/fgis-filters-panel/fgis-left-panel/div['
+                  '2]/div[2]/label')
+
+    # Нажимаем кнопку найти
+    wait_click(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/div/fgis-roei'
+                  '-verification-measuring-instruments-advanced-search/fgis-filters-panel/fgis-left-panel/div['
+                  '3]/div/button')
+
+    # Проверка даты в 2-х первых полях таблицы и получение итогового значения
+    count_poverka_ra = wait_date(d, my_date)
+
+    # Проверка на совпадение
+    if not count_poverka_ra == str(obsh):
+        messagebox.showwarning(f"Остановка программы {file}",
+                               f'Не сходятся значения кол-ва счетчиков\n'
+                               f'Количество в РА - {count_poverka_ra}\n'
+                               f'Количество черновиков в файле - {prom}\n'
+                               f'Общее количество поверок в файле - {obsh} \n')
+
+        print('Остановка программы в связи с расхождением кол-ва, ожидание действий пользователя')
+
+        time.sleep(100000)
+
+
+def download_file(d, my_date, obsh, prom, file):
+
+    # Сообщение для пользователя
+    print(f'Начинаю проверку публикации файла {file}')
+
+    # Переход в рабочую область
+    work_source(d)
+
+    time.sleep(2)  # ожидание
+
+    # Внесение с "Дата поверки"
+    wait_send_keys(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/div/fgis'
+                      '-roei-verification-measuring-instruments-advanced-search/fgis-filters-panel/fgis-left'
+                      '-panel/div[2]/div[2]/div/div[2]/fgis-date-range/div/fgis-calendar[1]/div/div/input',
+                   my_date)
+
+    # Внесение по "Дата поверки"
+    wait_send_keys(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/div/fgis'
+                      '-roei-verification-measuring-instruments-advanced-search/fgis-filters-panel/fgis-left'
+                      '-panel/div[2]/div[2]/div/div[2]/fgis-date-range/div/fgis-calendar[2]/div/div/input',
+                   my_date)
+
+    # Включить отображение кол-ва поверок
+    wait_click(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/div/fgis-roei'
+                  '-verification-measuring-instruments-advanced-search/fgis-filters-panel/fgis-left-panel/div['
+                  '2]/div[2]/label')
+
+    # Нажимаем кнопку найти
+    wait_click(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/div/fgis-roei'
+                  '-verification-measuring-instruments-advanced-search/fgis-filters-panel/fgis-left-panel/div['
+                  '3]/div/button')
+
+    # Проверка даты в 2-х первых полях таблицы и получение итогового значения
+    count_poverka_ra = wait_date(d, my_date)
+
+    # Проверка на совпадение
+    if not count_poverka_ra == str(obsh):
+        messagebox.showwarning(f"Остановка программы {file}",
+                               f'Не сходятся значения кол-ва счетчиков\n'
+                               f'Количество в РА - {count_poverka_ra}\n'
+                               f'Количество опубликованных в файле - {prom}\n'
+                               f'Общее количество поверок в файле - {obsh} \n')
+
+        print('Остановка программы в связи с расхождением кол-ва, ожидание действий пользователя')
+
+        time.sleep(100000)
+
+    # Сообщение пользователю
+    print(f'Проверка файла {file} пройдена успешно, приступаю к выгрузке файла из РА')
+
+    # Нажатие кнопки выгрузки
+    wait_click(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/div/div/div['
+                  '1]/fgis-table-toolbar/section/div/div[2]/fgis-toolbar/div/div[1]/fgis-toolbar-button/button')
+
+    # Выбор пункта excel
+    wait_click(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/fgis-export'
+                  '-settings-modal/fgis-modal/div/div[1]/div[2]/div[2]/p-radiobutton[2]/label')
+
+    # Нажатие на кнопку Экспорт
+    wait_click(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/fgis-export'
+                  '-settings-modal/fgis-modal/div/div[1]/div[3]/div/fgis-export-step/button[1]')
+
+    # Ожидание скачивания файла
+    wait_clickable(d, '/html/body/fgis-root/div/fgis-roei/fgis-roei-verification-measuring-instruments/div/fgis-roei'
+                      '-verification-measuring-instruments-advanced-search/fgis-filters-panel/fgis-left-panel/div['
+                      '2]/div[2]/div/div[1]/fgis-field-input/fgis-field-wrapper/div/div/input')
+
+    # Имя файла
+    name_file = f'Сведения по обеспечению единства измерений (Поверка СИ) от {str(datetime.datetime.today().strftime("%d.%m.%Y"))}.xlsx'
+
+    print(name_file)
+
+    # путь к скачанному файлу
+    path_to_file = f'C:/Users/{str(os.getlogin())}/Downloads/{name_file}'
+
+    # Ожидание появления файла
+    while not os.path.exists(path_to_file):
+        time.sleep(5)
+
+    # путь итог
+    new_path = f'end_ra_excel/{name_file.replace(".xlsx", f" - {file.split()[1]}")}'
+
+    # Перемещение законченного файла
+    shutil.move(path_to_file, new_path)
+
+    # Сообщение пользователю
+    print(f'Файл {new_path.split("/")[1]} выгружен успешно !!!')
+
+
+# ожидание появления номера счетчика в поиске
+def wait_clickable(d, path):
+    try:
+        WebDriverWait(d, 60).until(
+            EC.element_to_be_clickable((By.XPATH, path)))
+    except Te:
+        wait_clickable(d, path)
+
+
 # Функция обработки файла
 def one_rm(file_name, organization):
-
+    chern = 0
     publ_ra = 0
+    obsh = 0
 
     # Создание объекта класса Chrome
     driver = browse(organization)
@@ -430,12 +593,17 @@ def one_rm(file_name, organization):
     # Проверка на необходимость авторизации
     all_authorization(driver, organization)
 
+    f_name = file_name.split("/")[1]
+
     # Сообщение для пользователя
-    print(f'Начинаю перебор файла {file_name}')
+    print(f'Начинаю перебор файла {f_name}')
 
     # Открытие файла Excel
     wb = load_workbook(filename=file_name)
     sheet = wb.active
+
+    # Сообщение для пользователя
+    print(f'Приступаю к созданию черновиков для организации {organization}')
 
     # Проходим по строкам
     for row in sheet.iter_rows(min_row=2):
@@ -451,9 +619,36 @@ def one_rm(file_name, organization):
                 # Сообщение для пользователя
                 print(f'Черновик {row[0].value} сохранен для организации {organization}')
 
+            # Подсчет счетчиков со статусом Черновик РА
+            if row[6].value == 'Черновик РА':
+                chern += 1
+
+            # сбор общего кол-ва поверок
+            obsh += 1
+
+        else:
+            break
+
+    # Получение даты из имени файла
+    my_date = '.'.join(f_name.split()[1].split('.')[0:3])
+
+    # Сообщение для пользователя
+    print(f'Все сведения загружены, приступаю к проверке кол-ва черновиков для организации {organization}')
+
+    # Проверка количества счетчиков
+    check_kol(driver, my_date, obsh, chern, f_name)
+
+    # Сообщение для пользователя
+    print(f'Проверка прошла успешно, приступаю к публикации черновиков для организации {organization}')
+
+    # Проходим по строкам
+    for row in sheet.iter_rows(min_row=2):
+
+        # проверка на обязательное наличие сведений
+        if row[1].value:
+
             # Если статус = Черновик РА тогда приступаем к публикации сведения
             if row[6].value == 'Черновик РА':
-
                 time.sleep(3)
                 row[6].value = public_info(driver, row[0].value)  # публикуем запись
 
@@ -463,19 +658,21 @@ def one_rm(file_name, organization):
                 # Сообщение для пользователя
                 print(f'Сведение {row[0].value} опубликован для организации {organization}')
 
+            # Подсчет опубликованных поверок
             if row[6].value == 'Опубликовано в РА':
                 publ_ra += 1
-        else:
-            break
 
     # Закрытие рабочей книга
     wb.close()
 
     # Перемещение законченного файла
-    shutil.move(file_name, f'end/{file_name.split("/")[1]}')
+    shutil.move(file_name, f'end/{f_name}')
 
     # Ожидание
     time.sleep(5)
+
+    # Скачивание и перенос файла в необходимую папку
+    download_file(driver, my_date, obsh, publ_ra, f_name)
 
     # Закрытие браузера
     driver.close()
@@ -491,7 +688,7 @@ def print_end_publ(p, file):
         write_file.write(f'{name[0]}.{name[1]} - {p}\n')
 
         # Сообщение пользователю
-        print(f'Файл {file} завершен. Записал сведения о выгруженных поверках в файл log.txt')
+        print(f'Файл {file.split("/")[1]} завершен. Записал сведения о выгруженных поверках в файл log.txt')
 
 
 # Функция создания потока
@@ -506,7 +703,6 @@ def add_tread(excel):
 
 # Функция закрытия потоков
 def close_tread(tr_list):
-
     # Ожидание завершения процессов
     for tr in tr_list:
         tr.join()
@@ -540,16 +736,14 @@ if __name__ == '__main__':
 
     # Цикл прохода по списку кортежей
     for b in sp_tuple:
-        book_atm, book_mc = b
 
-        # Запуск процесса АТМ
-        thread_list.append(add_tread(book_atm))
+        # Цикл запуска потоков
+        for book_org in b:
+            # Запуск процесса
+            thread_list.append(add_tread(book_org))
 
-        # Ожидание перед запуском второго браузера
-        time.sleep(10)
-
-        # Запуск процесса МС
-        thread_list.append(add_tread(book_mc))
+            # Ожидание перед запуском последующего браузера
+            time.sleep(10)
 
         # Ожидание завершения процессов
         close_tread(thread_list)
